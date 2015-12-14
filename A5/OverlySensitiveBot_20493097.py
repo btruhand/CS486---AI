3a4e73f1f7d2734b1178f66e1e94e688a8f36c15
Btara Truhandarien <btara.truhandarien@gmail.com>
2015-12-13 23:13:56 -0500

    Do not initialize if already initialized

--- A5/OverlySensitiveBot_20493097.py
+++ A5/OverlySensitiveBot_20493097.py
@@ -2,24 +2,34 @@ from pdbot import PDBot
 import random
 
 class OverlySensitiveBot(PDBot):
+    INITIAL_GIVE_CHANCE = 0.35
+    
+    
+
     def __init__(self):
+        # indicate if the bot has already been initialized
+        self.first_init = False
         # initialize self
         self.init()    
 
     def init(self):
-        # hardcode give and take chance
-        self.give_chance = 0.35
+        if not self.first_init:
+            # if not yet initialied then initialize
+            # hardcode starting give chance
+            self.give_chance = 0.35
 
-        # trust point system, exponent of trusting_factor
-        self.trust_point = 0
-        # trusting factor, how much the bot
-        # trusts more for every give of the other player
-        self.trusting_factor = 1.1
+            # trust point system, exponent of trusting_factor
+            self.trust_point = 0
+            # trusting factor, how much the bot
+            # trusts more for every give of the other player
+            self.trusting_factor = 1.1
 
-        # hurt factor, how much trust is lost everytime
-        # the other player takes
-        self.hurt_factor = 2
+            # hurt factor, how much trust is lost everytime
+            # the other player takes
+            self.hurt_factor = 2
 
+            self.first_init = True
+    
     def get_play(self):
         if random.uniform(0,1) <= self.give_chance:
             # give for self.give_chance percent of the time
@@ -42,5 +52,3 @@ class OverlySensitiveBot(PDBot):
             # "reset" the chances the bot give and takes
             # causes the bot to be more inclined to take
             self.give_chance = (self.give_chance) / (self.give_chance + take_chance)
-
-        print self.give_chance, (1-self.give_chance)
